9f80c604135703ba2bb802f71f848ab98ab98ec4
Adriana Nunez <adri.nunhez@gmail.com>
2021-01-09 17:15:18 -0300

    V1 Calendario

---
+++ public/precache-manifest.8ba8b598142fa0eacc2e0305fa5d6ede.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da90e8aa83c36476ce7d3700d307cc08",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a3ffc7a37a41fc1ac2ee",
+    "url": "/static/css/2.ceee16bc.chunk.css"
+  },
+  {
+    "revision": "6f6202d0a9bae2d0a476",
+    "url": "/static/css/main.72ac0610.chunk.css"
+  },
+  {
+    "revision": "a3ffc7a37a41fc1ac2ee",
+    "url": "/static/js/2.98f9622c.chunk.js"
+  },
+  {
+    "revision": "590af14d9bb9b4b1926797b2a32e33a1",
+    "url": "/static/js/2.98f9622c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f6202d0a9bae2d0a476",
+    "url": "/static/js/main.6152f00b.chunk.js"
+  },
+  {
+    "revision": "7e829d7f4f2411c25d99",
+    "url": "/static/js/runtime-main.8c2230a5.js"
+  }
+]);
